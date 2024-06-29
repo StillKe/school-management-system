a7ec9816508d6b4f9b77818ff8b2d9304d362452
mamasb <pkpipimwangi@gmail.com>
2024-06-29 04:34:54 +0300

    the side menu hides when closed an appeas when opened

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ function openModule(moduleName) {
   // Show the selected module
   document.getElementById(moduleName).classList.add("active");
 }
+// Function to open the sidebar
+function openSidebar() {
+  document.getElementById("sidebar").style.left = "0";
+  document.getElementsByClassName("main-content")[0].style.marginLeft = "150px";
+  document.querySelector(".open-btn").style.display = "none"; // Hide open button
+}
+
+// Function to close the sidebar
+function closeSidebar() {
+  document.getElementById("sidebar").style.left = "-300px";
+  document.getElementsByClassName("main-content")[0].style.marginLeft = "0";
+  document.querySelector(".open-btn").style.display = "block"; // Show open button
+}
